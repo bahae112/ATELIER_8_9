f53684b435aa37e5022b078a55a388c3a028ad10
bahae112 <bahaeaouanet2004@gmail.com>
2025-01-31 23:40:33 +0100

    added

---
+++ view/bienvenue.php
@@ -0,0 +1,11 @@
+<?php
+include "../model/utilisateur.php";
+session_start();
+if(isset($_SESSION['utilisateur'])){
+	echo "bienvenue " .$_SESSION['utilisateur']."<br>";
+	echo "<a href='connexion.html'>deconnexion</a>";
+}
+else{
+	header('location:connexion.html');
+}
+?>
